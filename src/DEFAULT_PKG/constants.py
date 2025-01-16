ad60236c9096cc67a50202306a32b2dfadddbf16
Ishan Gaur <ishang@berkeley.edu>
2025-01-16 23:11:40 +0000

    experiment folder --> data folder

--- src/DEFAULT_PKG/constants.py
+++ src/DEFAULT_PKG/constants.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
 HOME_FOLDER = Path(__file__).parent.parent.parent
-EXPERIMENTS_FOLDER = HOME_FOLDER / "experiments"
+DATA_FOLDER = HOME_FOLDER / "data"
 DB_FOLDER = HOME_FOLDER / "db"
